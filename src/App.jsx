69c985c3003a7e69ec28173b1cb3e81c7dbfd892
siriyerrabelly <siriyerrabelly@gmail.com>
2026-01-08 08:34:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+
+export default function App() {
+  const student = {
+    name: "Siri",
+    regNo: "2500032474",
+    dept: "CSE",
+    year: "II Year",
+    
+  };
+
+  return (
+    <div className="page">
+      <h1 className="title"> My profile</h1>
+
+      <div className="card">
+        <div>
+          <img src="profile.png"></img>
+        </div>
+       
+
+        <h2 className="name">{student.name}</h2>
+        <p className="sub">Reg No: <b>{student.regNo}</b></p>
+
+        
+           <div className="box">
+            <p className="label">Department</p>
+            <p className="value">{student.dept}</p>
+          </div>
+          <div className="box">
+            <p className="label">Year</p>
+            <p className="value">{student.year}</p>
+          </div>
+          
+          <div className="box">
+            <p className="label">i am interested in paintings and craft </p>
+            <p className="value">{student.cgpa}</p>
+          </div>
+        </div>
+
+       
+      </div>
+    
+  );
+}
